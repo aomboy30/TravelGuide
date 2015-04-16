97479d33bc8ee64f7bc545837180cdbcead3f284
Amporn Sonthi <ampornsonthi@Amporns-Mac-mini.local>
2015-04-16 23:39:32 +0700

    Initial Commit

---
+++ TravelGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravelGuide
+//
+//  Created by Amporn Sonthi on 4/16/2558 BE.
+//  Copyright (c) 2558 Amporn Sonthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravelGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravelGuide
+//
+//  Created by Amporn Sonthi on 4/16/2558 BE.
+//  Copyright (c) 2558 Amporn Sonthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
